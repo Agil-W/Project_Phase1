ccbe62a421e2d587cb80d7e01e79e8d2d5921168
Agil-W <150829830+Agil-W@users.noreply.github.com>
2024-03-02 19:30:35 +0800

    play speed function , display time ,volume and stop and gui - by Edison

--- playback.py
+++ playback.py
@@ -1,5 +1,7 @@
 import pyaudio
 import struct
+import threading
+
 
 class WAVReader:
     def __init__(self, filename):
@@ -15,25 +17,30 @@ class WAVReader:
             riff_header = file.read(12)
             format_chunk_header = file.read(24)
             # format_chunk_header[0:4] == 'fmt '
-            format_chunk_size = struct.unpack('<I', format_chunk_header[4:8])[0]
+            format_chunk_size = struct.unpack(
+                '<I', format_chunk_header[4:8])[0]
             type_of_format = struct.unpack('<H', format_chunk_header[8:10])[0]
-            self.num_channels = struct.unpack('<H', format_chunk_header[10:12])[0]
-            self.sample_rate = struct.unpack('<I', format_chunk_header[12:16])[0]
-            self.bits_per_sample = struct.unpack('<H', format_chunk_header[22:24])[0]
+            self.num_channels = struct.unpack(
+                '<H', format_chunk_header[10:12])[0]
+            self.sample_rate = struct.unpack(
+                '<I', format_chunk_header[12:16])[0]
+            self.bits_per_sample = struct.unpack(
+                '<H', format_chunk_header[22:24])[0]
 
             # skip 'data'
             file.read(8)
 
             # Read the audio data
             data = file.read()
-            num_samples = len(data) // (self.bits_per_sample // 8)  # Assuming 16-bit audio
+            # Assuming 16-bit audio
+            num_samples = len(data) // (self.bits_per_sample // 8)
 
             # Unpack the audio data into a list of samples
             self.audio_data = struct.unpack('<{}h'.format(num_samples), data)
 
     def set_filename(self, filename):
         self.filename = filename
-    
+
     def get_filename(self):
         return self.filename
 
@@ -51,29 +58,93 @@ class WAVReader:
 
 
 class SoundPlayer:
-    def __init__(self, wav_reader):
+    def __init__(self, wav_reader, progress_bar, time):
         self.wav_reader = wav_reader
+        self.progress_bar = progress_bar
+        # self.seek_bar_value = seek_value
+        self.time = time
         self.playback_speed = 1.0
+        self.volume = 1.0
+        self.is_playing = False
+        self.stop_flag = threading.Event()  # Add a threading Event
+
+    def get_total_time(self):
+        return len(self.wav_reader.get_audio_data())/self.wav_reader.get_sample_rate()
 
     def set_playback_speed(self, speed):
         self.playback_speed = speed
 
-    def play_sound(self):
-        p = pyaudio.PyAudio()
+    def set_volume(self, volume):
+        self.volume = volume
 
-        # Open an audio stream
-        stream = p.open(format=p.get_format_from_width(self.wav_reader.get_bits_per_sample()//8), 
-                        channels=self.wav_reader.get_num_channels(),
-                        rate=int(self.wav_reader.get_sample_rate() * self.playback_speed),
-                        output=True)
+    def play_sound(self, start_time=0):
+        if self.is_playing:  # only one thread can play sound
+            return
 
-        # Play the audio data
-        audio_data = self.wav_reader.get_audio_data()
-        for sample in audio_data:
-            stream.write(struct.pack('<h', sample))
+        def play_sound_thread():
+            self.p = pyaudio.PyAudio()
+            self.is_playing = True
+            adjust_sample_rate = int(
+                self.wav_reader.get_sample_rate() * self.playback_speed)
 
-        # Stop and close the audio stream
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
+            if adjust_sample_rate > 48000:
+                adjust_sample_rate = 48000
+            elif adjust_sample_rate < 0:
+                adjust_sample_rate = 44100
 
+            self.stream = self.p.open(format=self.p.get_format_from_width(self.wav_reader.get_bits_per_sample()//8),
+                                      channels=self.wav_reader.get_num_channels(),
+                                      rate=int(self.wav_reader.get_sample_rate()
+                                               * self.playback_speed),
+                                      output=True)
+
+            start_sample = int(start_time * self.wav_reader.get_sample_rate())
+            audio_data = self.wav_reader.get_audio_data()[start_sample:]
+            self.total_samples = len(audio_data)
+            progress = 0
+            full_time = len(self.wav_reader.get_audio_data()
+                            ) / self.wav_reader.get_sample_rate()
+            for sample in audio_data:
+                adjusted_volume_sample = int(sample * self.volume)
+                if adjusted_volume_sample < -32768:
+                    adjusted_volume_sample = -32768
+                elif adjusted_volume_sample > 32767:
+                    adjusted_volume_sample = 32767
+
+                if self.stop_flag.is_set():
+                    break
+
+                self.stream.write(struct.pack('<h', adjusted_volume_sample))
+
+                progress += 1
+                if progress % 100 == 0:  # update every 1000 samples
+
+                    print(f'Progress: {progress/self.total_samples*100:.2f}%')
+                    progress_time = progress / self.wav_reader.get_sample_rate()
+                    print(f'Time: {progress_time:.2f}s')
+                    # Update the progress bar
+                    self.progress_bar['value'] = progress / \
+                        self.total_samples*100
+                    self.time.set(f'{progress_time:.3f}/{full_time:.3f}')
+
+            # no one can see this code
+            self.progress_bar['value'] = 100
+            self.time.set(f'{full_time:.3f}/{full_time:.3f}')
+            self.stream.stop_stream()
+            self.stream.close()
+            self.p.terminate()
+            self.is_playing = False
+
+            if not self.stop_flag.is_set():
+                self.stream.stop_stream()
+                self.stream.close()
+                self.p.terminate()
+                self.is_playing = False
+
+            self.stop_flag.clear()  # Clear the stop flag at the end
+        threading.Thread(target=play_sound_thread).start()
+
+    def stop_sound(self):
+        if self.is_playing:
+            self.stop_flag.set()  # Set the stop flag when stopping
+            self.is_playing = False
--- recorder_gui.py
+++ recorder_gui.py
@@ -1,39 +1,86 @@
 import tkinter as tk
 import os
+import threading
+import time
+
+from tkinter import ttk
 from recorder import AudioRecorder
 from playback import SoundPlayer, WAVReader
-import threading
+
 
 class RecorderApp:
     def __init__(self):
         self.root = tk.Tk()
+        self.root.geometry('1000x700')
         self.root.title("Audio Recorder")
         self.root.resizable(False, False)
 
-        self.listbox = tk.Listbox(self.root)
-        self.listbox.pack()
+        self.listbox = tk.Listbox(self.root, width='25', height='35')
+        self.listbox.grid(row='0', column='0', sticky='NW')
         self.load_recorded_files()
 
-        self.label = tk.Label(text= "00:00:00")
-        self.label.pack()
+        self.display_panel = tk.Frame(
+            self.root, width='950', height='563', bg='blue')
+        self.display_panel.grid(row='0', column='1')
 
-        self.record_button = tk.Button(self.root, text="⏺", font=("Arial", 40, "bold"), command=self.toggle_recording)
-        self.record_button.pack()
+        self.control_panel = tk.Frame(self.root, width='1000', height='150')
+        self.control_panel.grid(row='1', columnspan='2')
 
-        self.play_button = tk.Button(self.root, text="⏯", font=("Arial", 40, "bold"), command=self.play_audio)
-        self.play_button.pack()
+        self.progress_bar = ttk.Progressbar(
+            self.display_panel, length='650', orient='horizontal', maximum='100')
+        self.progress_bar.place(x='150', y='520')
+        self.progress_bar.bind("<Button-1>", self.seek)
 
+        '''self.seek_bar_value = tk.DoubleVar()
+        self.seek_bar = tk.Scale(
+            self.display_panel, variable=self.seek_bar_value, orient='horizontal', length='650')
+        self.seek_bar.place(x='150', y='520')'''
+
+        self.time = tk.StringVar()
+        self.time_stamp = tk.Label(self.display_panel, width='10', textvariable=self.time, font=(
+            "Arial", 11, "bold"))
+        self.time_stamp.place(x='21', y='520')
+        # button
+        self.record_button = tk.Button(self.control_panel, text="⏺", font=(
+            "Arial", 25, "bold"), relief='solid', command=self.toggle_recording)
+        self.record_button.place(x='0', y='35')
+
+        self.play_button = tk.Button(self.control_panel, text="⏯", font=(
+            "Arial", 25, "bold"), relief='solid', command=self.play_audio)
+        self.play_button.place(x='95', y='35')
+
+        self.stop_button = tk.Button(self.control_panel, text="⏹", font=(
+            "Arial", 25, "bold"), relief='solid', command=self.stop_audio)
+        self.stop_button.place(x='195', y='35')
+
+        self.volume = tk.Scale(
+            self.control_panel, orient='horizontal', width='25', length='150', from_='0', to='100', command=self.set_volume)
+        self.volume.set(1)
+        self.volume.place(x='750', y='50')
+
+        self.vol_label = tk.Label(self.control_panel, text="Volume")
+        self.vol_label.place(x='750', y='33')
+
+        self.play_speed = tk.Scale(
+            self.control_panel, orient='horizontal', width='25', length='150', from_='0.1', to='8', resolution='0.1', command=self.set_play_speed)  # max is 8
+        self.play_speed.set(1)
+        self.play_speed.place(x='545', y='50')
+
+        self.play_speed_label = tk.Label(self.control_panel, text="Play Speed")
+        self.play_speed_label.place(x='545', y='33')
+        # Define the function for the timer
         self.recorder = AudioRecorder()
         self.WAVReader = WAVReader(None)
-        self.player = SoundPlayer(self.WAVReader)
-
+        self.player = SoundPlayer(
+            self.WAVReader, self.progress_bar, self.time)
         self.root.mainloop()
-    
+
     def load_recorded_files(self):
-        recorded_files = [file for file in os.listdir("recorded_files") if file.endswith(".wav")]
+        recorded_files = [file for file in os.listdir(
+            "recorded_files") if file.endswith(".wav")]
 
         recorded_files = sorted(recorded_files)
-        
+
         self.listbox.delete(0, tk.END)
 
         for file in recorded_files:
@@ -41,15 +88,16 @@ class RecorderApp:
 
     def toggle_recording(self):
         if not self.recorder.recording:  # Start recording if not already recording
-            self.record_button.config(text="⏹",fg="red")
+            self.record_button.config(text="⏹", fg="red")
             threading.Thread(target=self.recorder.record_audio).start()
+            self.start_time = time.time()
         else:  # Stop recording if already recording
             self.record_button.config(text="⏺", fg="black")
             self.recorder.stop_recording()
-            self.recorder.save_as_wav()  
+            self.recorder.save_as_wav()
             self.load_recorded_files()
 
-    def play_audio(self):
+    def play_audio(self, start_time=0):
         if not self.listbox.curselection():
             return
         selected_file = self.listbox.get(self.listbox.curselection())
@@ -59,6 +107,28 @@ class RecorderApp:
         self.WAVReader.read_wav_file()
         self.player.play_sound()
 
-RecorderApp()
+    def stop_audio(self):
+        self.player.stop_sound()
 
-    
+    def set_play_speed(self, speed):
+        self.player.playback_speed = float(speed)
+
+    def set_volume(self, volume):
+        self.player.set_volume(float(volume))
+
+    def is_recording(self):
+        return self.recording
+
+    def seek(self, event):
+        # Calculate the new start time based on the mouse click position
+        ratio = event.x / self.progress_bar.winfo_width()
+        new_start_time = ratio * self.player.get_total_time()
+
+        # Stop the current playback
+        self.player.stop_sound()
+
+        # Start playing from the new start time
+        self.player.play_sound(start_time=new_start_time)
+
+
+RecorderApp()
